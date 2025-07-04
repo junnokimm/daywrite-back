b961e11cbd32e9bf0d6318c0358b43eb26e4bf34
co9mltjs <co9mltjs@gmail.com>
2025-07-04 14:37:03 +0900

    필사 데이터베이스 추가

--- models/main/scriptSchema.js
+++ models/main/scriptSchema.js
@@ -4,6 +4,7 @@ const scriptSchema = new mongoose.Schema({
   content: { type: String, required: true },
   book: { type: String, required: true },
   author: { type: String, required: true },
+  publisher: { type: String },
   publishedDate: { type: String },
   bookCover: { type: String }, // 이미지 URL
   keyword: [{ type: String }],
