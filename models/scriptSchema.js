51f9d537f55aace980a1c6130f28b07d656c8d38
co9mltjs <co9mltjs@gmail.com>
2025-07-04 14:04:55 +0900

    데이터 모델 정의

---
+++ models/scriptSchema.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const scriptSchema = new mongoose.Schema({
+  content: { type: String, required: true },
+  book: { type: String, required: true },
+  author: { type: String, required: true },
+  publishedDate: { type: String },
+  bookCover: { type: String }, // 이미지 URL
+  keyword: [{ type: String }],
+  genre: [{ type: String }],
+});
+
+export default mongoose.model("Script", scriptSchema);
