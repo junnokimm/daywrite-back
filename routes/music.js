44e32be5733c9f4b5db0cdef61e2236054b9165a
junnokimm <nexta1023@gmail.com>
2025-07-14 21:21:12 +0900

    [김준호]rootRouter.js app.js 수정

--- routes/music.js
+++ routes/music.js
@@ -1,10 +1,8 @@
-// routes/music.js
 import express from "express";
 import { getMusicByKeywordAndGenre } from "../controllers/musicController.js";
 
-const router = express.Router();
+const musicRouter = express.Router();
 
-router.post("/recommend", getMusicByKeywordAndGenre);
+musicRouter.post("/recommend", getMusicByKeywordAndGenre);
 
-export default router;
-
+export default musicRouter;
--- routes/rootRouter.js
+++ routes/rootRouter.js
@@ -1,11 +1,9 @@
 import express from "express";
 import userRouter from "./user/userRouter.js";
-import musicRouter from "./music.js"
-import router from "./music.js";
+import musicRouter from "./music.js"; 
 
 const rootRouter = express.Router();
 
-rootRouter.use("/users/api", userRouter)
-router.use("/api/music", musicRouter)
+rootRouter.use("/users/api", userRouter);
 
 export default rootRouter;
